65c8499acd4866849172b007c632330dd99d3a4e
albertovdasilva <68698081+albertovdasilva@users.noreply.github.com>
2022-06-22 14:13:42 -0400

    Update ListData.cs

--- FunctionApp/SharePoint/ListData.cs
+++ FunctionApp/SharePoint/ListData.cs
@@ -40,8 +40,7 @@ namespace Plumsail.DataSource.SharePoint
 
             var queryOptions = new List<QueryOption>()
             {
-                new QueryOption("select", "id"),
-                new QueryOption("filter", $"fields/Title eq '{req.Query["accountnumber"]}'"),
+                new QueryOption("select", "id"),                
                 new QueryOption("expand", "fields(select=Title,Author)")
             };
             var itemsPage = await list.Items
